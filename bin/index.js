b957a45f6d5ba9613a49af81e69f716f978f52ed
been <been1730137@163.com>
2019-07-23 22:21:03 +0800

    add commander

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const fs = require('fs')
+
+program
+    .command('list')
+    .description('list files in current working directory.')
+    .option('-a, --all', 'Whether to display hidden files')
+    .action( options => {
+        console.log(options)
+        fs.readdir( process.cwd(), (err, files) => {
+            let list = files
+            if(!options.all){
+                list = files.filter( file => {
+                    return !file.startsWith('.')
+                })
+            }
+            console.log(list.join('\n\r'))
+        })
+    })
+
+program.parse( process.argv )
